9631680759ba30475d43ff51ab6b5ee94687be27
Vitalii Hoshchenko <viposha@gmail.com>
2023-02-27 15:29:58 +0200

    add movies_list with template

--- cinema/movie/urls.py
+++ cinema/movie/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home
+from .views import home, Movies
 
 
 urlpatterns = [
 	path('', home, name='home'),
+	path('movies/', Movies.as_view(), name='movies')
 ]
--- cinema/movie/views.py
+++ cinema/movie/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Movie
 
 
 def home(request):
 	return render (request, 'movie/home.html')
+
+
+class Movies(ListView):
+	model = Movie
+	template_name = 'movie/movie_list.html'
+	context_object_name = 'movies'
+
+	def get_context_data(self, *, object_list=None, **kwargs):
+		context = super().get_context_data(**kwargs)
+		return context
+
+	def get_queryset(self):
+		return Movie.objects.all()
