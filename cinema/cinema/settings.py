f847b3af37f5d203e21012a3c31a0a6d4c3bbb32
Vitalii Hoshchenko <Viposha@gmail.com>
2024-10-16 00:51:24 +0300

    fix staticdirs

--- cinema/cinema/settings.py
+++ cinema/cinema/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'cinema/static'), os.path.join(BASE_DIR, 'cinema/cinema/static'), '/root/projects/djcinema/cinema/static']
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'cinema/static'),]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
